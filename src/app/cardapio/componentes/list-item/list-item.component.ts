def1eb5b4b56ea262647c31c22b0100b858cca0a
unknown <andherrson@hotmail.com>
2021-03-28 01:57:13 -0300

    Primeiro commit

--- src/app/cardapio/componentes/list-item/list-item.component.ts
+++ src/app/cardapio/componentes/list-item/list-item.component.ts
@@ -10,10 +10,18 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ListItemComponent implements OnInit {
 
   @Input() lancheValorPromocao:LancheValorPromocao;
+  descricao: String = "";
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.lancheValorPromocao && this.lancheValorPromocao.lanche) {
+      this.configurarDescricaoIngredientes();
+    }
+  }
+
+  configurarDescricaoIngredientes() {
+    this.descricao = this.lancheValorPromocao.lanche.ingredientes.map(item => item.nome).join(', ');
   }
 
 }
